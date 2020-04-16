81470808e6f5ea905c7a266384f25edeb279f24f
Virgil Martinez <virgilmartinez@gmail.com>
2020-04-15 19:23:44 -0500

    save point
    Added three enemies. Working on wave spawn now.

---
+++ BlackThornProd/Top Down Shooter/Assets/Scripts/FlyingEnemyBullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class FlyingEnemyBullet : MonoBehaviour
+{
+    private Player _playerScript;
+    private Vector2 _targetPosition;
+
+    public float speed;
+    public int damage;
+
+    private void Start()
+    {
+        _playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        _targetPosition = _playerScript.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, _targetPosition) > .1f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            _playerScript.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
